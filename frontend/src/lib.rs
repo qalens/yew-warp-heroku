1c135360f670e0c19099519607f4810c24d51c8a
ATM <naik.atmaram@gmail.com>
2021-02-07 20:35:07 +0530

    ATM | changed template

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,23 +1,19 @@
-#![recursion_limit = "1024"]
-#![allow(clippy::large_enum_variant)]
-#![allow(clippy::eval_order_dependence)]
+#![recursion_limit = "512"]
 
-pub mod app;
-pub mod components;
-pub mod routes;
+mod app;
 
 use wasm_bindgen::prelude::*;
 
-use app::App;
-
-// Use `wee_alloc` as the global allocator.
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // This is the entry point for the web app
 #[wasm_bindgen]
-pub fn run() -> Result<(), JsValue> {
+pub fn run_app() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::start_app::<App>();
+    yew::start_app::<app::App>();
     Ok(())
 }
